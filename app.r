97b5eb60a8bb0e3585090e573a0c72c97ccdf596
Danny Gibbons <danegibbons@gmail.com>
2025-07-17 16:36:40 -0400

    Fixing gui organization

--- app.r
+++ app.r
@@ -2,29 +2,41 @@ library(shiny)
 library(bslib)
 
 # Define UI ----
-ui <- page_sidebar(
-  title = "Beatty Graph Display Tool",
-  actionButton("files.load", "Load Files"), 
-  tableOutput("file.list"),
-  colourInput("color.pick", "Select Color", "black"),
-  actionButton("color.load", "Select Color"),
-  tableOutput("color.list")
-  plotOutput("plot.out")
+ui <- page_fluid(
+  titlePanel("Beatty Graph Display Tool"),
+  layout_columns(
+    col_width = 2,
+    card(
+      card_header("Files"),
+      actionButton("files.load", "Load Files"), 
+      tableOutput("file.list"),
+    ),
+    card(
+      card_header("Colors"),
+    # colourInput("color.pick", "Select Color", "black"),
+      actionButton("color.load", "Select Color"),
+      tableOutput("color.list"),
+    ),
+    card(
+      card_header("Plot"),
+    plotOutput("plot.out"),
+    )
+  )
 )
 
 # Define server logic ----
 server <- function(input, output) {
   
-  names(data_folder) <- c("File Names")
-  output$file.list <- renderTable(data_folder, striped = TRUE) 
-  
-  output$color.list <- renderTable(graph_colors, striped = TRUE)
-  output$plot.out <- renderPlot({ 
-    mpg |> 
-      ggplot(aes(hwy, cty)) + 
-      geom_point() + 
-      labs(title = "Click anywhere on the plot")
-  }) 
+  # names(data_folder) <- c("File Names")
+  # output$file.list <- renderTable(data_folder, striped = TRUE) 
+  # 
+  # output$color.list <- renderTable(graph_colors, striped = TRUE)
+  # output$plot.out <- renderPlot({ 
+  #   mpg |> 
+  #     ggplot(aes(hwy, cty)) + 
+  #     geom_point() + 
+  #     labs(title = "Click anywhere on the plot")
+  # }) 
 }
 
 # Run the app ----
