8c055fdac0715a040d9e2ceab1c35dbbf6ee9735
Danny Gibbons <danegibbons@gmail.com>
2025-07-22 16:50:14 -0400

    GUI Widget Alignment

--- app.r
+++ app.r
@@ -1,31 +1,69 @@
 library(shiny)
 library(bslib)
+library(colourpicker)
 
 # Define UI ----
-ui <- page_fluid(
+ui <- page_fillable(
   titlePanel("Beatty Graph Display Tool"),
   layout_columns(
-    col_width = 2,
+    width=1/2,
     card(
       card_header("Files"),
-      actionButton("files.load", "Load Files"), 
+      # actionButton("files.load", "Load Files"), 
+      fileInput("file1", "Choose TXT Files", accept = ".txt",multiple = TRUE),
       tableOutput("file.list"),
     ),
+
     card(
       card_header("Colors"),
-    # colourInput("color.pick", "Select Color", "black"),
-      actionButton("color.load", "Select Color"),
+      colourInput("color.pick", NULL, "black",palette="limited"),
+      # actionButton("color.load", "Select Color"),
       tableOutput("color.list"),
-    ),
+    )),
+  layout_columns(
+    width=1,
     card(
       card_header("Plot"),
+    actionButton("generate.plot","Generate Plot"),
     plotOutput("plot.out"),
-    )
+
+      layout_columns(
+        width=1/2,
+    textInput( 
+      "out.file.name", 
+      "Export File Name", 
+      placeholder = "Enter name (no .ext)..."
+    ),
+    selectInput( 
+      "select", 
+      "Select Export File Option Below:", 
+      list("EPS" = ".eps", "PS" = ".ps", "PDF" = ".pdf", "JPEG"=".jpg","TIFF"=".tiff","PNG"=".png","BMP"=".bmp","SVG"=".svg") 
+    ), 
+    ),
+
+    ),
   )
 )
 
 # Define server logic ----
 server <- function(input, output) {
+  # file <- input$file1
+  # ext <- tools::file_ext(file$datapath)
+  # req(file)
+  # validate(need(ext == "txt", "Please upload a .txt file"))
+  # all.data.files <- reactive({
+  #   for (each.file in input$file1){
+  #     print(each.file)
+  #   }
+  # })  file <- input$file1
+  # ext <- tools::file_ext(file$datapath)
+  # req(file)
+  # validate(need(ext == "txt", "Please upload a .txt file"))
+  # all.data.files <- reactive({
+  #   for (each.file in input$file1){
+  #     print(each.file)
+  #   }
+  # })
   
   # names(data_folder) <- c("File Names")
   # output$file.list <- renderTable(data_folder, striped = TRUE) 
